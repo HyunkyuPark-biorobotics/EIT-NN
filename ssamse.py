1801f59b25c98e5b229879ddaf04d042a7739bdb
HyunkyuPark <44401853+HyunkyuPark-biorobotics@users.noreply.github.com>
2020-06-05 22:01:26 +0900

    Update ssamse.py

--- ssamse.py
+++ ssamse.py
@@ -4,12 +4,12 @@ import keras.backend as K
 from utils import load_dataset
 
 
-def SSAMSE(laplace=True):
+def SSAMSE(SSAMSE_switch=True):
 	kernel = load_dataset("./spatial_sensitivity_matrix.csv")
 	kernel = tf.convert_to_tensor(kernel, dtype=tf.float32)
 
 	def loss(y_true, y_pred):
-		if laplace:
+		if SSAMSE_switch:
 			y_true = tf.matmul(y_true, kernel)
 		return K.mean(K.square(y_true - y_pred))
 
